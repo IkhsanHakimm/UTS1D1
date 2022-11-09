c370acc5c319e63919990969cae5cbd1bb8cec8c
Ikhsan <xsanhakim14@gmail.com>
2022-11-09 14:58:29 +0700

    sudah selesai

--- src/JawabanNomerTiga.java
+++ src/JawabanNomerTiga.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class JawabanNomerTiga {
+public class JawabanNomerTiga {
+    public static void main(String[] args) {
+        System.out.println(" Hello Class 1D ");
+        for(int i=0;i<10;i++){
+            System.out.println(" Aku ");
+        }
+    }
 }
